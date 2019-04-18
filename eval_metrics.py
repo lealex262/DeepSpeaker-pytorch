Fixed eval_metric bug and change to average loss

eval_metrics.py
@@ -7,10 +7,7 @@ def evaluate(distances, labels):
     thresholds = np.arange(0, 30, 0.01)
     tpr, fpr, accuracy = calculate_roc(thresholds, distances,
         labels)
-    thresholds = np.arange(0, 30, 0.001)
-    val,  far = calculate_val(thresholds, distances,
-        labels, 1e-3)
-    return tpr, fpr, accuracy, val,  far
+    return tpr, fpr, accuracy
 
 
 def calculate_roc(thresholds, distances, labels):

model.py
@@ -36,8 +36,8 @@ class TripletMarginLoss(Function):
         d_n = self.pdist.forward(anchor, negative)
 
         dist_hinge = torch.clamp(self.margin + d_p - d_n, min=0.0)
-        loss = torch.sum(dist_hinge)
-        # loss = torch.mean(dist_hinge)
+        # loss = torch.sum(dist_hinge)
+        loss = torch.mean(dist_hinge)
         return loss
 
 

train_triplet.py
@@ -381,7 +381,7 @@ def train(train_loader, model, optimizer, epoch):
     labels = np.array([sublabel for label in labels for sublabel in label])
     distances = np.array([subdist for dist in distances for subdist in dist])
 
-    tpr, fpr, accuracy, val, far = evaluate(distances,labels)
+    tpr, fpr, accuracy = evaluate(distances, labels)
     print('\33[91mTrain set: Accuracy: {:.8f}\n\33[0m'.format(np.mean(accuracy)))
     logger.log_value('Train Accuracy', np.mean(accuracy))
 
@@ -424,7 +424,7 @@ def test(test_loader, model, epoch):
     labels = np.array([sublabel for label in labels for sublabel in label])
     distances = np.array([subdist for dist in distances for subdist in dist])
 
-    tpr, fpr, accuracy, val,  far = evaluate(distances,labels)
+    tpr, fpr, accuracy = evaluate(distances, labels)
     print('\33[91mTest set: Accuracy: {:.8f}\n\33[0m'.format(np.mean(accuracy)))
     logger.log_value('Test Accuracy', np.mean(accuracy))
 